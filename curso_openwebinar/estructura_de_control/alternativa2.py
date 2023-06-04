9dfddeb47de3e624ab6a0a5277278ea986b47411
wanapito <ivannr20@gmail.com>
2023-06-04 10:38:04 +0200

    1

---
+++ curso_openwebinar/estructura_de_control/alternativa2.py
@@ -0,0 +1,8 @@
+edad = int(input("dime tu edad:"))
+if edad >= 18:
+    print("eres mayor de edad ")
+
+# como hay varias alternativas se utiliza el formato if else
+else:
+    print("eres menor de edad")
+print("programa terminado")
---
+++ curso_openwebinar/estructura_de_control/alternativas01.py
@@ -0,0 +1,5 @@
+edad = int(input("dime tu edad:"))
+if edad >= 18:
+    #la alternativa esta debajo de if con tablulador
+    print("eres mayor de edad")
+print("programa terminado")
